7e5a5f2d50e4d750f390e54ad9341718551c8930
SntDxlxd <105860224+SntDxlxd@users.noreply.github.com>
2022-12-24 09:57:09 +0300

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from config import Config
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.config.from_object(Config)
+login = LoginManager(app)
+login.login_view = 'login'
+bootstrap = Bootstrap(app)
+
+from app.routes import app
